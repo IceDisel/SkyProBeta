1f839849633796adf2dec379b61a855b3582bb1e
Vladimir Titov <tvv-vg@yandex.ru>
2023-10-26 21:42:35 +0300

    add docstring

--- src/widget.py
+++ src/widget.py
@@ -4,6 +4,11 @@ from src.masks import get_mask_the_bank_account, get_mask_the_bankcard
 
 
 def get_mask_bankcard_account(number_name: str) -> str:
+    """
+    принимает на вход строку информацией тип карты/счета и номер карты/счета
+    :return: возвращает эту строку с замаскированным номером карты/счета.
+    """
+
     split_number_name = number_name.split()
 
     if "Счет" in split_number_name:
@@ -15,6 +20,11 @@ def get_mask_bankcard_account(number_name: str) -> str:
 
 
 def convert_date(date_string: str) -> str:
+    """
+    принимает на вход строку, вида "2018-07-11T02:26:18.671407"
+    :return: возвращает строку с датой в виде "11.07.2018"
+    """
+
     date_obj = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
     formatted_date = date_obj.strftime("%d.%m.%Y")
     return formatted_date
