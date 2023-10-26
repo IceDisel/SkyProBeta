a6e843a0025b53da189394c3e69c3b17dd1a0fab
Vladimir Titov <tvv-vg@yandex.ru>
2023-10-26 20:52:02 +0300

    init

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
-from src.masks import get_mask_the_bank_account, get_mask_the_bankcard
+from src.widget import convert_date, get_mask_bankcard_account
 
 
 def main():
     """Данный код для проверки"""
-    print(get_mask_the_bankcard("7000 7922 8960 6361"))
+    print(get_mask_bankcard_account("Visa Classic 6831982476737658"))
+    print(get_mask_bankcard_account("Maestro 1596837868705199"))
+    print(get_mask_bankcard_account("MasterCard 7158300734726758"))
+    print(get_mask_bankcard_account("Счет 35383033474447895560"))
 
-    print(get_mask_the_bank_account("73654108430135874305"))
+    print(convert_date("2018-07-11T02:26:18.671407"))
 
 
 if __name__ == "__main__":
