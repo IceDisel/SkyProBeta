dcde1bf0e226bac973348cf77a05ca8c46da698f
Vladimir Titov <tvv-vg@yandex.ru>
2023-10-26 20:51:43 +0300

    add def get_mask_bankcard_account

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,20 @@
 from datetime import datetime
 
+from src.masks import get_mask_the_bank_account, get_mask_the_bankcard
 
-def convert_date(date_string):
+
+def get_mask_bankcard_account(number_name: str) -> str:
+    split_number_name = number_name.split()
+
+    if "Счет" in split_number_name:
+        mask_bank_account = get_mask_the_bank_account(split_number_name[-1])
+        return ' '.join(split_number_name[:-1]) + " " + mask_bank_account
+    else:
+        mask_bankcard = get_mask_the_bankcard(split_number_name[-1])
+        return ' '.join(split_number_name[:-1]) + " " + mask_bankcard
+
+
+def convert_date(date_string: str) -> str:
     date_obj = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
     formatted_date = date_obj.strftime("%d.%m.%Y")
     return formatted_date
