93632f1c230f63138ec0205f61328c03ac3d8c62
Vladimir Titov <tvv-vg@yandex.ru>
2023-11-10 22:38:56 +0300

    add Annotations

--- src/decorators.py
+++ src/decorators.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from typing import Any, Callable, Optional
 
 
-def log(filename=None):
-    def wrapper(func):
-        def inner(*args, **kwargs):
+def log(filename: Optional[str] = None) -> Callable:
+    def wrapper(func: Callable) -> Callable:
+        def inner(*args: Any, **kwargs: Any) -> Any:
+
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{datetime.now()} {func.__name__} ok\n"
@@ -14,12 +16,12 @@ def log(filename=None):
                     print(log_message)
                 return result
             except Exception as err:
-                log_message = f"{datetime.now()} {func.__name__} error: {type(err).__name__}. Inputs: {args}, {kwargs}\n"
+                log_mess = f"{datetime.now()} {func.__name__} error: {type(err).__name__}. Inputs: {args}, {kwargs}\n"
                 if filename:
                     with open(filename, "a") as file:
-                        file.write(log_message)
+                        file.write(log_mess)
                 else:
-                    print(log_message)
+                    print(log_mess)
 
         return inner
 
