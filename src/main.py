ed2d80dba2383ed46e1d03b2d1b43709feaf1ecf
Vladimir Titov <tvv-vg@yandex.ru>
2023-11-12 18:03:48 +0300

    refactor code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from data.data import TRANSACTIONS
-from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+# from data.data import TRANSACTIONS
+# from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+# from src.masks import get_mask_the_bankcard
+
 
 # from src.processing import dictionary_sorted, get_dictionary_search
 
@@ -12,19 +14,19 @@ def main() -> None:
     # print()
     # print(dictionary_sorted(DATA_LIST, False))
     # [print(item) for item in dictionary_sorted(DATA_LIST)]
-
-    usd_transactions = filter_by_currency(TRANSACTIONS, "USD")
-
-    for _ in range(2):
-        print(next(usd_transactions)["id"])
-
-    descriptions = transaction_descriptions(TRANSACTIONS)
-
-    for _ in range(5):
-        print(next(descriptions))
-
-    for card_number in card_number_generator(1, 5):
-        print(card_number)
+    #
+    # usd_transactions = filter_by_currency(TRANSACTIONS, "USD")
+    #
+    # for _ in range(2):
+    #     print(next(usd_transactions)["id"])
+    #
+    # descriptions = transaction_descriptions(TRANSACTIONS)
+    #
+    # for _ in range(5):
+    #     print(next(descriptions))
+    #
+    # for card_number in card_number_generator(1, 5):
+    #     print(card_number)
 
 
 if __name__ == "__main__":
