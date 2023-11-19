64bbf02def46e5bb7986f1821337f0475f0326a5
Vladimir Titov <tvv-vg@yandex.ru>
2023-11-19 20:09:56 +0300

    add logger in functions

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def read_json_file(path: str) -> list[dict]:
@@ -13,10 +16,13 @@ def read_json_file(path: str) -> list[dict]:
         with open(path, encoding="utf-8") as file_json:
             data = json.load(file_json)
             if isinstance(data, list):
+                logger.info("JSON-файл успешно прочитан")
                 return data
             else:
+                logger.warning("JSON-файл не содержит список данных")
                 return []
     except (FileNotFoundError, UnicodeError, json.JSONDecodeError):
+        logger.exception("Произошла ошибка при чтении JSON-файла")
         return []
 
 
@@ -27,6 +33,8 @@ def get_transaction_amount_rub(transaction: dict) -> float:
     :return: float сумму транзакции (amount) в рублях
     """
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        logger.info("Транзакция выполнена в рублях")
         return float(transaction["operationAmount"]["amount"])
     else:
+        logger.error("Транзакция выполнена не в рублях")
         raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях.")
