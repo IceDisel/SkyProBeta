a56d62caf96922b62dbeadc75bb9f289e028dadf
Vladimir Titov <tvv-vg@yandex.ru>
2023-11-24 17:39:50 +0300

    refactor

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from src import log
-from src.masks import get_mask_the_bank_account, get_mask_the_bankcard
-from src.settings import PATH_JSON
-from src.utils import get_transaction_amount_rub, read_json_file
+from src.settings import PATH_CSV, PATH_JSON, PATH_XLSX
+from src.utils import get_transaction_amount_rub, read_csv_xlsx_file, read_json_file
 
 # from src.processing import dictionary_sorted, get_dictionary_search
 logger = log.setup_logging()
@@ -13,12 +12,15 @@ def main() -> None:
     print(read_json_file(PATH_JSON))
     transaction = read_json_file(PATH_JSON)
     print(get_transaction_amount_rub(transaction[0]))
+    print(transaction[0])
 
-    get_mask_the_bankcard("7895 456 85469 4526")
-    get_mask_the_bankcard("7895 456 85469 452")
+    print("=================-----------")
 
-    get_mask_the_bank_account("42569854756958452365")
-    get_mask_the_bank_account("425698547569584523658")
+    print(read_csv_xlsx_file(PATH_CSV)[1])
+
+    print("======++++++++++===========")
+
+    print(read_csv_xlsx_file(PATH_XLSX)[998])
 
 
 if __name__ == "__main__":
